672f47a81176b5c7b60e7bcd46bb7bbc271a8ba4
crypto49er <jcntest2011@gmail.com>
2019-03-03 00:46:46 -0500

    Updated RSI_Candle to smart strat

--- strategies/RSI_Candle_v2.js
+++ strategies/RSI_Candle_v2.js
@@ -13,8 +13,9 @@
 // Sell when RSI > 70 
 // Sell when 1% stop loss
 
-var log = require('../core/log');
-var config = require ('../core/util.js').getConfig();
+const fs = require('fs');
+const log = require('../core/log');
+const config = require ('../core/util.js').getConfig();
 
 const CandleBatcher = require('../core/candleBatcher');
 const RSI = require('../strategies/indicators/RSI.js');
@@ -28,19 +29,20 @@ var rsi5 = new RSI({ interval: 14 });
 var sma5 = new SMA(200);
 var advised = false;
 var rsi5History = [];
-var wait = 0;
 var counter = 0;
 var disableTrading = false;
-var waitForRejectedRetry = 0;
 var priceHistory = [];
 var sma5History = [];
 var highestRSI = 0; // highestRSI in last 5 candles
 var candle5 = {};
+var fiatLimit = 100;
+var assetLimit = 0.74062586; // $100 USD if ETH is ~$135
 
 // Prepare everything our method needs
 strat.init = function() {
   this.name = "RSI + Candle";
   this.requiredHistory = config.tradingAdvisor.historySize;
+  this.tradeInitiated = false;
 
   // since we're relying on batching 1 minute candles into 5 minute candles
   // lets throw if the settings are wrong
@@ -58,6 +60,33 @@ strat.init = function() {
   // Add an indicator even though we won't be using it because
   // Gekko won't use historical data unless we define the indicator here
   this.addIndicator('rsi', 'RSI', { interval: this.settings.interval});
+
+  fs.readFile(this.name + '-balanceTracker.json', (err, contents) => {
+    var fileObj = {};
+    if (err) {
+      log.warn('No file with the name', this.name + '-balanceTracker.json', 'found. Creating new tracker file');
+      fileObj = {
+        assetLimit: assetLimit,
+        fiatLimit: fiatLimit,
+      };
+      fs.appendFile(this.name + '-balanceTracker.json', JSON.stringify(fileObj), (err) => {
+        if(err) {
+          log.error('Unable to create balance tracker file');
+        }
+      });
+    } else {
+      try {
+        fileObj = JSON.parse(contents)
+        assetLimit = fileObj.assetLimit;
+        fiatLimit = fileObj.fiatLimit;
+      }
+      catch (err) {
+        log.error('Tracker file empty or corrupted');
+      }
+    }
+  });
+
+  log.info('Buy Limit', fiatLimit, 'Sell Limit', assetLimit);
 }
 
 // What happens on every new candle?
@@ -68,17 +97,15 @@ strat.update = function(candle) {
   this.batcher5.write([candle]);
   this.batcher5.flush();
 
-  // Send message 
+  // Send message that bot is still working after 24 hours (assuming minute candles)
   counter++;
   if (counter == 1440){
-    log.remote(this.name, ' - Bot is still working.');
+    if (lastTraded){
+      log.remote(this.name, ' - Bot is still working. \n Last Trade:', lastTraded);
+    } else {
+      log.remote(this.name, ' - Bot is still working.');
+    }
     counter = 0;
-  }
-
-  // Decrement wait
-  if (wait > 0) {
-    wait--;
-    log.debug('Wait: ', wait);
   }
 
 }
@@ -125,40 +152,130 @@ strat.update5 = function(candle) {
 strat.check = function() {
 
   // Buy when RSI < 12 and RSI dropped more than 18 points compared to previous 2 candles
-  if (rsi5.result < 12 && (rsi5History[7] > rsi5.result + 18 || rsi5History[8] > rsi5.result + 18 ) && !advised && !disableTrading){
-      this.buy('Buy because RSI less than 12');
+  if (rsi5.result < 12 && (rsi5History[7] > rsi5.result + 18 || rsi5History[8] > rsi5.result + 18 ) 
+  && !advised && !disableTrading && !this.tradeInitiated){
+    log.info('Buy because RSI less than 12');
+    this.advice({ 
+      direction: 'long',
+      amount: currency > fiatLimit ? fiatLimit : currency,
+    });
+    advised = true;
+    return;
   }
 
   
   // //Buy when RSI < 30 and candle is a hammer
-  if (rsi5.result < 30 && candle5.open > candle5.low && candle5.open - candle5.low > candle5.low * 0.006 && candle5.open > candle5.close && (candle5.open - candle5.close)/(candle5.open - candle5.low) < 0.25 && !advised && !disableTrading){
-    this.buy('Buy because RSI less than 30 and candle is a hammer');
+  if (rsi5.result < 30 && candle5.open > candle5.low && candle5.open - candle5.low > candle5.low * 0.006 
+    && candle5.open > candle5.close && (candle5.open - candle5.close)/(candle5.open - candle5.low) < 0.25 && !advised && !disableTrading && !this.tradeInitiated){
+    log.info('Buy because RSI less than 30 and candle is a hammer');
+    this.advice({ 
+      direction: 'long',
+      amount: currency > fiatLimit ? fiatLimit : currency,
+    });
+    advised = true;
+    return;
   }
 
   // Sell when RSI > 70
-  if (rsi5.result > 70 && advised) {
-    this.sell('Take Profit - RSI past 70');
+  if (rsi5.result > 70 && advised && !this.tradeInitiated) {
+    log.info('Take Profit - RSI past 70');
+    this.advice({
+      direction: 'short',
+      amount: asset > assetLimit ? assetLimit : asset,
+    });
+    advised = false;
+    return;
   }
 
   // Sell if currentPrice <= buyPrice * 0.99 (1% stop loss)
-  if (currentPrice <= buyPrice * 0.99 && advised){
-    this.sell('Stop Loss - 1% loss');
+  if (currentPrice <= buyPrice * 0.99 && advised && !this.tradeInitiated){
+    log.info('Stop Loss - 1% loss');
+    this.advice({
+      direction: 'short',
+      amount: asset > assetLimit ? assetLimit : asset,
+    });
+    advised = false;
+    return;
   } 
 
 }
 
-strat.sell = function(reason) {
-  this.advice('short');
-  log.info(reason);
-  advised = false;
-  buyPrice = 0;
+// This is called when trader.js initiates a 
+// trade. Perfect place to put a block so your
+// strategy won't issue more trader orders
+// until this trade is processed.
+strat.onPendingTrade = function(pendingTrade) {
+  this.tradeInitiated = true;
+
 }
 
-strat.buy = function(reason) {
-  this.advice('long');
-  log.info(reason);
-  advised = true;
-  buyPrice = currentPrice;
+
+// This runs whenever a trade is completed
+// as per information from the exchange.
+// The trade object looks like this:
+// {
+//   id: [string identifying this unique trade],
+//   adviceId: [number specifying the advice id this trade is based on],
+//   action: [either "buy" or "sell"],
+//   price: [number, average price that was sold at],
+//   amount: [number, how much asset was trades (excluding "cost")],
+//   cost: [number the amount in currency representing fee, slippage and other execution costs],
+//   date: [moment object, exchange time trade completed at],
+//   portfolio: [object containing amount in currency and asset],
+//   balance: [number, total worth of portfolio],
+//   feePercent: [the cost in fees],
+//   effectivePrice: [executed price - fee percent, if effective price of buy is below that of sell you are ALWAYS in profit.]
+// }
+strat.onTrade = function(trade) {
+  this.tradeInitiated = false;
+  
+  if (trade.action == 'buy') {
+    assetLimit = fiatLimit / trade.price;
+  }
+
+  if (trade.action == 'sell') {
+    fiatLimit = trade.amount * trade.price;
+  }
+  var fileObj = {
+    assetLimit: assetLimit,
+    fiatLimit: fiatLimit,
+  }
+  fs.writeFile(this.name + '-balanceTracker.json', JSON.stringify(fileObj), (err) => {
+    if(err) {
+      log.error('Unable to write to balance tracker file');
+    }
+  });
+
+  lastTraded = trade.date.format('l LT');
+}
+
+// Trades that didn't complete with a buy/sell
+strat.onTerminatedTrades = function(terminatedTrades) {
+  log.info('Trade failed. Reason:', terminatedTrades.reason);
+  this.tradeInitiated = false;
+}
+
+// This runs whenever the portfolio changes
+// including when Gekko starts up to talk to 
+// the exhange to find out the portfolio balance.
+// This is how the portfolio object looks like:
+// {
+//   currency: [number, portfolio amount of currency],
+//   asset: [number, portfolio amount of asset],
+// }
+strat.onPortfolioChange = function(portfolio) {
+
+  // Sell if we start out holding a bag
+  // We determine this as currency and asset starts out
+  // at 0 before we get the info from the exchange. 
+  // if (asset == 0 && currency == 0 && portfolio.asset > 0) {
+  //   log.info('Starting with a sell as Gekko probably crashed after a buy')
+  //   //this.advice('short');
+  // }
+
+  asset = portfolio.asset;
+  currency = portfolio.currency;
+
 }
 
 strat.onCommand = function(cmd) {
@@ -183,11 +300,11 @@ strat.onCommand = function(cmd) {
     }
   if (command == 'buy') {
   cmd.handled = true;
-  this.buy('Manual buy order from telegram');
+  log.info('Manual buy/sell disabled');
   }
   if (command == 'sell') {
   cmd.handled = true;
-  this.sell('Manual sell order from telegram');
+  log.info('Manual buy/sell disabled');
   }
   if (command == 'stop') {
     cmd.handled = true;
